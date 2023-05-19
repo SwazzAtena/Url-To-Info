950f95ab2f2d456a3af8e811894c0d59b3cce61b
Mr.Ülg3n <68772806+SwazzAtena@users.noreply.github.com>
2023-05-19 20:51:44 +0300

    Update UrlToİnf.py

--- UrlToİnf.py
+++ UrlToİnf.py
@@ -1,11 +1,13 @@
 import socket
 import os
+os.system("pip install ip2geotools")
 from ip2geotools.databases.noncommercial import DbIpCity
 from colorama import Fore , init
 print(Fore.LIGHTMAGENTA_EX)
 print("Gerekli Yüklemeler Yapılıyor Biraz Uzun Sürebilir")
 os.system("pip install colorama")
 os.system("pip install ip2geotools")
+print(Fore.LIGHTRED_EX)
 url = input("Site Adresini Giriniz : ")
 ipadres = socket.gethostbyname(url)
 print("By Ömer Tuğrul Bayram :) ")
